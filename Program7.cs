f86f76179a48157b860395a4467e42d391638c48
sashasuro <surovskaasasa@gmail.com>
2023-02-08 00:07:48 +0300

    Create Program7.cs
    Program7.cs

---
+++ Program7.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //T1
+            //int[] omas = new int[8];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.Write($"omas [{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas [{i}]={omas[i]}");
+            //}
+
+
+            //T2
+            //double[] omas = { -6, 7.8, -5.6, 3, 12, 9.3 };
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    if (omas[i] > 0)
+            //    {
+            //        Console.WriteLine(omas[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //T3
+            //int[] omas = { -7, 10, 5, 16, -4, 18 };
+            //Console.WriteLine("Вывод массива:");
+            //for ( int i = omas.Length-1; i>=0; i--)
+            //{
+            //    Console.WriteLine(omas[i]);
+            //}
+
+
+            //T4
+            //int[] omas = new int[10];
+            //Random random = new Random();
+            //for (int i = 0; i <= 9; i++)
+            //{
+            //    omas[i] = random.Next(162, 191);
+            //    Console.WriteLine(omas[i]);
+            //}
+        }
+    }
+}
